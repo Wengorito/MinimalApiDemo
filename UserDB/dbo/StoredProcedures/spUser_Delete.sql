70be96b09acead01ad3ad0ccf02e900c0919510b
Grzegorz <hvitr.ulfr@gmail.com>
2023-11-01 14:12:22 +0100

    Created User tables and CRUD stored procedures.

---
+++ UserDB/dbo/StoredProcedures/spUser_Delete.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Delete]
+	@Id int
+AS
+BEGIN
+	DELETE
+	FROM [User]
+	WHERE Id = @Id;
+END
---
+++ UserDB/dbo/StoredProcedures/spUser_GetAll.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetAll]
+
+AS
+BEGIN
+	SELECT Id, FirstName, LastName
+	FROM [User];
+END
